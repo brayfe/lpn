6dbd57d8035aa2f0fdf567941e7d68d7dc86f871
markfullmer <mfullmer@gmail.com>
2017-08-22 13:55:55 -0500

    Add comments & fix syntax on LayoutPerNodeManager

--- src/Services/LayoutPerNodeManagerService.php
+++ src/Services/LayoutPerNodeManagerService.php
@@ -4,24 +4,35 @@ namespace Drupal\layout_per_node\Services;
 
 use Drupal\node\Entity\Node;
 
+/**
+ * Handles saving data to the node entity.
+ */
 class LayoutPerNodeManagerService {
+
   /**
+   * Main functional method: given a node ID & layout array, save configuration.
+   *
    * @param string $nid
+   *   The node ID.
    * @param array $layout_data
-   * This should be an array with layout information.
-   * Example: Add a block to the 'first' and 'second' region of 'twocol' layout:
-   * $layout_data = [
-   *   'twocol' => [
-   *     'first' => [
-   *       '71393b1f-d47c-49df-9d58-c2343cf0f931' => "block_content",
-   *     ],
-   *     'second' => [
-   *       '2834fc70-714b-4352-9c11-eedf72a1893e' => "block_content",
-   *    ]
-   * ];
+   *   The layout array:
+   *   Example: Add a block to the 'first' & 'second' region of 'twocol' layout:
+   *   $layout_data = [
+   *     'twocol' => [
+   *       'first' => [
+   *         '71393b1f-d47c-49df-9d58-c2343cf0f931' => "block_content",
+   *       ],
+   *       'second' => [
+   *         '2834fc70-714b-4352-9c11-eedf72a1893e' => "block_content",
+   *      ]
+   *   ];
+   *   End notes on layout array.
+   *
    * @return bool
+   *   Whether the layout was successfully saved or not.
    */
-  public function updateContent($nid, $layout_data) {
+  public function updateContent($nid, array $layout_data) {
+
     $node = Node::load($nid);
     if ($node && !empty($layout_data)) {
       // Prepare data sent via AJAX POST request for storage on the node.
@@ -46,11 +57,12 @@ class LayoutPerNodeManagerService {
       $node->setRevisionLogMessage('Layout updated');
       $node->setRevisionTranslationAffected(TRUE);
       $node->save();
-      return $node;
+      return TRUE;
     }
     else {
-      \Drupal::logger('layout_per_node')->warning(t("Warning: Attempted to update @nid layout_per_node data and was unable to find existing record", [@nid => $nid]));
+      \Drupal::logger('layout_per_node')->warning(t("Warning: Attempted to update @nid layout_per_node data and was unable to find existing record", ['@nid' => $nid]));
       return FALSE;
     }
   }
+
 }
